0592a3bb7d62c96678c23c8f75ed8752c1982ce7
Alan <alaanoriel@gmail.com>
2023-04-28 17:30:16 -0300

    Botones

--- ProyectoProgra3/AppArticulos/Form1.cs
+++ ProyectoProgra3/AppArticulos/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace AppArticulos
 {
-    public partial class Form1 : Form
+    public partial class Ventana_Principal : Form
     {
-        public Form1()
+        public Ventana_Principal()
         {
             InitializeComponent();
         }
