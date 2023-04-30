811b20b54f6c52eac4d20b3defa353a2410c5658
Alan <alaanoriel@gmail.com>
2023-04-30 14:30:23 -0300

    Ventana Agregar y Modificar

---
+++ ProyectoProgra3/AppArticulos/VentanaAgregar.Designer.cs
@@ -0,0 +1,47 @@
+﻿
+namespace AppArticulos
+{
+    partial class VentanaAgregar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // VentanaAgregar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 423);
+            this.Name = "VentanaAgregar";
+            this.Text = "VentanaAgregar";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
