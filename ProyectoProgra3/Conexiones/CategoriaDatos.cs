059f6ce6c81f639ffdf6a3b621a2549791928ae5
AgustinVaras <Agustin.Varas@outlook.com.ar>
2023-05-04 22:46:38 -0300

    Creacion de la clase CategoriaDatos

---
+++ ProyectoProgra3/Conexiones/CategoriaDatos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clases;
+
+namespace Conexiones
+{
+    public class CategoriaDatos
+    {
+        public List<Categoria> Listar()
+        {
+            List<Categoria> Listar = new List<Categoria>();
+            AccesoSQL Datos = new AccesoSQL();
+
+            try
+            {
+                Datos.Consulta("Select Id, Descripcion as Desc From CATEGORIAS");
+                Datos.EjecutarLectura();
+
+                while (Datos.Lector.Read())
+                {
+                    Categoria aux = new Categoria();
+                    aux.Id = (int)Datos.Lector["Id"];
+                    aux.Descripcion = (string)Datos.Lector["Desc"];
+                }
+                return Listar;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                Datos.CerrarConexion();
+            }
+        }
+    }
+}
